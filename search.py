9ea8a26da412d9de9cb4dbcdc98e0f18891795c4
Simon Kutuzov <computer016@gmail.com>
2018-06-13 23:48:18 +0300

    Added basic index for txt corpus

--- search.py
+++ search.py
@@ -1,14 +1,12 @@
 from inverted_index import InvertedIndex
 import os
-#just test commit
+
 
 def main():
     os.chdir('./std_docs_txt')
     docs = os.listdir(os.getcwd())
     index = InvertedIndex(docs)
     index.build()
-
-    print(index.description_index)
 
 
 if __name__ == '__main__':
