2ac2f82d91a371203f4c89836796f7ab23c812db
Simon Kutuzov <computer016@gmail.com>
2018-06-14 00:13:22 +0300

    Updated project skeleton

---
+++ query_handler.py
@@ -0,0 +1,8 @@
+class QueryHandler:
+
+    def __init__(self, index):
+        self.index = index
+
+    def loop(self):
+        # Handle incoming queries here
+        pass
--- search.py
+++ search.py
@@ -1,12 +1,18 @@
-from inverted_index import InvertedIndex
 import os
+from inverted_index import InvertedIndex
+from query_handler import QueryHandler
+
+RELATIVE_PATH_TO_CORPUS = './std_docs_txt'
 
 
 def main():
-    os.chdir('./std_docs_txt')
+    os.chdir(RELATIVE_PATH_TO_CORPUS)
+
     docs = os.listdir(os.getcwd())
     index = InvertedIndex(docs)
     index.build()
+
+    QueryHandler(index).loop()
 
 
 if __name__ == '__main__':
