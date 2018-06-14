3c3d65119176a391ddf91f8c17646dc4a0128151
AndrewPirozhkov <anree97@gmail.com>
2018-06-14 17:21:33 +0300

    add separated words by '.' in the index

--- inverted_index.py
+++ inverted_index.py
@@ -10,17 +10,27 @@ class InvertedIndex:
         self.corpus = corpus
         self.description_index = defaultdict(set)
 
+    def read_file(self, file):
+        with open(file) as f:
+            words = word_tokenize(f.read())
+            buf = []
+            for word in words:
+                split_word = word.split('.')
+                if len(split_word) > 1:
+                    buf.extend(split_word)
+            words.extend(buf)
+        return words
+
     def build(self):
         stemmer = PorterStemmer()
         stop_words = set(stopwords.words('english'))
 
         for i, file in enumerate(self.corpus):
-            with open(file) as f:
-                words = word_tokenize(f.read())
-                for word in words:
-                    if word not in stop_words:
-                        stemmed_word = stemmer.stem(word)
-                        self.description_index[stemmed_word].add(i)
+            words = self.read_file(file)
+            for word in words:
+                if word not in stop_words:
+                    stemmed_word = stemmer.stem(word)
+                    self.description_index[stemmed_word].add(i)
 
     def get_postings(self, word):
         return self.description_index[word]
